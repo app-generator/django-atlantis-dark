840e517e4ccb556355810f098ac60c3c032cb5f9
shalin-i <shalini.sharma@bigbasket.com>
2022-02-03 19:27:09 +0530

    BB-102996 changed

--- apps/home/views.py
+++ apps/home/views.py
@@ -64,11 +64,24 @@ def store(request):
             for sheet in xls.sheet_names:
                 print(sheet)
                 if(sheet!='Summary'):
-                    df = pd.read_excel(xls,sheet,header=9)
+                    df = pd.read_excel(xls,sheet)
+                    row_start_1=0
+                    for row in range(df.shape[0]): 
+                        for col in range(df.shape[1]):
+                            if df.iat[row,col] == 'sl_no':
+                                row_start_1 = row
+                                break
+                    #,header=9)
+                    df = pd.read_excel(xls,sheet,row_start=(row_start_1-1))
                     if(df.empty):
-                        continue
-                    else:
-                        if('API' in sheet):
+                            continue
+                    flag=0
+                    for col_name in df.columns: 
+                        print(col_name)
+                        if('API' in col_name or 'api' in col_name):
+                            flag=1
+                            break
+                    if(flag==1):
                             df.columns = ['sl_no','api_type','priority','Dependencies','Description','service_name','owner_poc','mock_link','curl','dependent_services','dependent_spoc','dev_status','open_q','status','test_owner','jira','i_comm']
                             user = settings.DATABASES['default']['USER']
                             password = settings.DATABASES['default']['PASSWORD']
@@ -80,18 +93,18 @@ def store(request):
                             )
                             engine = create_engine(database_url, echo=False)
                             df.to_sql('home_apicase', con=engine,  if_exists='append', chunksize = 1000, index= False)
-                        else:
-                            df.columns = ['sl_no','d_s','pre_req','t_scenario','exp_result','exp_plat','a_exe','b_status','c_date','d_owner','e_id','f_auto','g_aown','h_poc','i_comm']
-                            user = settings.DATABASES['default']['USER']
-                            password = settings.DATABASES['default']['PASSWORD']
-                            database_name = settings.DATABASES['default']['NAME']
-                            database_url = 'mysql://{user}:{password}@localhost/{database_name}'.format(
-                            user=user,
-                            password=password,
-                            database_name=database_name,
-                            )
-                            engine = create_engine(database_url, echo=False)
-                            df.to_sql('home_test', con=engine,  if_exists='append',chunksize = 1000, index= False)
+                    else:
+                        df.columns = ['sl_no','d_s','pre_req','t_scenario','exp_result','exp_plat','a_exe','b_status','c_date','d_owner','e_id','f_auto','g_aown','h_poc','i_comm']
+                        user = settings.DATABASES['default']['USER']
+                        password = settings.DATABASES['default']['PASSWORD']
+                        database_name = settings.DATABASES['default']['NAME']
+                        database_url = 'mysql://{user}:{password}@localhost/{database_name}'.format(
+                        user=user,
+                        password=password,
+                        database_name=database_name,
+                        )
+                        engine = create_engine(database_url, echo=False)
+                        df.to_sql('home_test', con=engine,  if_exists='append',chunksize = 1000, index= False)
             # df2.save_to_database(
             #     model=[apicase],
             #     mapdict=['sl_no','api_type','priority','Dependencies','Description','service_name','owner_poc','mock_link','curl','dependent_services','dependent_spoc','dev_status','open_q','status','test_owner','jira','i_comm']
