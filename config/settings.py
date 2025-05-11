b490d79f52ef33f091e291ce0fb81130d7540cb0
Mominur Rahman <101915710+mominur-helios@users.noreply.github.com>
2025-05-11 21:39:31 +0600

    Settings/SECRET_KEY: remove randomness if not specified

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
+SECRET_KEY = os.environ.get('SECRET_KEY', 'Super_Secr3t_9999')
 
 # Render Deployment Code
 DEBUG = 'RENDER' not in os.environ
